e713de678e3aa090a6aa054d222e49faf92e1cdf
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2022-11-07 16:02:55 +0600

    large change

--- lib/main.dart
+++ lib/main.dart
@@ -30,71 +30,72 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              padding: const EdgeInsets.only(left: 40, top: 50, right: 20),
-              height: 350,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [Color(0xff3383cd), Color(0xff11249f)]),
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/virus.png"))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                      alignment: Alignment.topRight,
-                      child: SvgPicture.asset("assets/icons/menu.svg")),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                      child: Stack(
-                    children: [
-                      SvgPicture.asset("assets/icons/Drcorona.svg",
-                          width: 230,
-                          fit: BoxFit.fitWidth,
-                          alignment: Alignment.topCenter),
-                      const Positioned(
-                          top: 20,
-                          left: 150,
-                          child: Text(
-                            'All you need\nStay at home.',
-                            style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white),
-                          )),
-                      Container(),
-                    ],
-                  ))
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                padding: const EdgeInsets.only(left: 40, top: 50, right: 20),
+                height: 350,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [Color(0xff3383cd), Color(0xff11249f)]),
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/virus.png"))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset("assets/icons/menu.svg")),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                        child: Stack(
+                      children: [
+                        SvgPicture.asset("assets/icons/Drcorona.svg",
+                            width: 230,
+                            fit: BoxFit.fitWidth,
+                            alignment: Alignment.topCenter),
+                        const Positioned(
+                            top: 20,
+                            left: 150,
+                            child: Text(
+                              'All you need\nStay at home.',
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            )),
+                        Container(),
+                      ],
+                    ))
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(color: const Color(0xffe5e5e5))),
-            child: Row(
-              children: [
-                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
-                const SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: DropdownButton(
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(color: const Color(0xffe5e5e5))),
+              child: Row(
+                children: [
+                  SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: DropdownButton(
                       isExpanded: true,
                       underline: const SizedBox(),
                       icon: SvgPicture.asset("assets/icons/dropdown.svg"),
@@ -112,37 +113,166 @@ class MyHomePage extends StatelessWidget {
                           child: Text(value),
                         );
                       }).toList(),
-                      onChanged: (value){},
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      RichText(
+                          text: const TextSpan(children: [
+                        TextSpan(
+                            text: "Case Update\n",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff303030))),
+                        TextSpan(
+                            text: "Newest update March 28",
+                            style: TextStyle(color: Color(0xFF959595))),
+                      ])),
+                      const Spacer(),
+                      const Text(
+                        "See details",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF3382CC)),
                       ),
-                )
-              ],
-            ),
-          ),
-          const SizedBox(height: 20,),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    RichText(text: const TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "Case Update\n",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color(0xff303030))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              offset: const Offset(0, 4),
+                              blurRadius: 30,
+                              color: const Color(0xFFB7B7B7).withOpacity(.16))
+                        ]),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        NewWidget(
+                          number: 1050,
+                          color: Color(0xFFFF8748),
+                          title: 'infected',
                         ),
-                        TextSpan(
-                          text: "Newest update March 28",
-                          style: TextStyle(color: Color(0xFF959595))
+                        NewWidget(
+                          number: 87,
+                          color: Color(0xFFFF4848),
+                          title: 'deaths',
+                        ),
+                        NewWidget(
+                          number: 46,
+                          color: Color(0xFF36C12C),
+                          title: 'recovered',
                         )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'spread of virus',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF303030)),
+                      ),
+                      Text(
+                        "See Details",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF3382CC)),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.all(20),
+                    height: 178,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0,10),
+                          blurRadius: 30,
+                          color: Color(0xFFB7B7B7).withOpacity(.16)
+                        ),
                       ]
-                    ) )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                    ),
+                    child: Image.asset('assets/images/map.png', fit: BoxFit.contain,),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key? key,
+    required this.number,
+    required this.color,
+    required this.title,
+  }) : super(key: key);
+
+  final int number;
+  final Color color;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle, color: color.withOpacity(.26)),
+          child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                border: Border.all(width: 2, color: color)),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          "$number",
+          style: TextStyle(fontSize: 40, color: color),
+        ),
+        Text(
+          title,
+          style: const TextStyle(fontSize: 16, color: Color(0xFF959595)),
+        )
+      ],
     );
   }
 }
